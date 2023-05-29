a2f3e34b771a48ebb52088b339d43581b8c6fb73
Olivier <sarl.muller@cegetel.net>
2023-05-29 17:15:19 +0200

    Description update

--- solTherm.ino
+++ solTherm.ino
@@ -1,23 +1,11 @@
 /*
-  Analog input, analog output, serial output
+  This code is to control a Solar Thermal Panel installation
 
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
+  There are 3 thermal sensors to get :
+  - Solar panel temperature
+  - Solar storage temperature
+  - ECS temperature (Eau Chaude Sanitaire)
 
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
-
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
 */
 
 #include <ESP8266WiFi.h>
